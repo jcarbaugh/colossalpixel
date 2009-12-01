2f336f1d088b0292f229cff4b6b3be1e3adce81a
Jeremy Carbaugh <jcarbaugh@gmail.com>
2009-11-30 23:47:52 -0500

    test view only in debug

--- urls.py
+++ urls.py
@@ -1,9 +1,17 @@
 from django.conf.urls.defaults import *
+from django.conf import settings
 
-urlpatterns = patterns('',
-    url(r'^(?P<rgb>[0-9a-fA-F]{3}|[0-9a-fA-F]{6}).png$', 'colossalpixel.views.png', name='png'),
-    url(r'^(?P<rgb>[0-9a-fA-F]{3}|[0-9a-fA-F]{6})/$', 'colossalpixel.views.rgb', name='rgb'),
-    url(r'^test/$', 'colossalpixel.views.test', name='test'),
-    url(r'^$', 'colossalpixel.views.index', name='index'),
-    url(r'^(?P<rgb>.*)', 'colossalpixel.views.notacolor', name='notacolor'),
+urlpatterns = patterns('colossalpixel.views',
+    url(r'^(?P<rgb>[0-9a-fA-F]{3}|[0-9a-fA-F]{6}).png$', 'png', name='png'),
+    url(r'^(?P<rgb>[0-9a-fA-F]{3}|[0-9a-fA-F]{6})/$', 'rgb', name='rgb'),
+    url(r'^$', 'index', name='index'),
+)
+
+if settings.DEBUG:
+    urlpatterns += patterns('django.views.generic.simple',
+        url(r'test/', 'direct_to_template', {'template': 'colossalpixel/test.html'})
+    )
+
+urlpatterns += patterns('colossalpixel.views',
+    url(r'^(?P<rgb>.*)', 'notacolor', name='notacolor'),
 )
